3dc7aef414c72d88d9dfa6fd8f9ef05039310373
yevhen_povzlo <yevhen.povzlo@sitecore.com>
2020-05-29 12:03:24 +0300

    Fix the relation between Rendering Parameters and standard values, which is lost after clicking OK buttons in Control Properties and Layout Details pop-ups without modifying appropriate parameters

---
+++ src/Sitecore.Support.414532/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.414532")]
+[assembly: AssemblyProduct("Sitecore.Support.414532")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.414532/SetValues.cs
@@ -0,0 +1,78 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentManager.ReturnFieldEditorValues
+{
+    using Sitecore;
+    using Sitecore.Configuration;
+    using Sitecore.Data;
+    using Sitecore.Reflection;
+    using Sitecore.Shell.Applications.ContentEditor;
+    using Sitecore.Shell.Applications.ContentManager;
+    using Sitecore.Shell.Applications.ContentManager.ReturnFieldEditorValues;
+    using System.Collections.Generic;
+    using System.Web.UI;
+
+    public class SetValues
+    {
+        public void Process(ReturnFieldEditorValuesArgs args)
+        {
+            #region patch 414532
+            List<FieldDescriptor> unchangedFields = new List<FieldDescriptor>();
+            #endregion
+            foreach (FieldInfo info in args.FieldInfo.Values)
+            {
+                Control control = Context.ClientPage.FindSubControl(info.ID);
+                if (control != null)
+                {
+                    string str;
+                    if (control is IContentField)
+                    {
+                        string[] values = new string[] { (control as IContentField).GetValue() };
+                        str = StringUtil.GetString(values);
+                    }
+                    else
+                    {
+                        str = StringUtil.GetString(ReflectionUtil.GetProperty(control, "Value"));
+                    }
+                    if (str != "__#!$No value$!#__")
+                    {
+                        string str2 = info.Type.ToLowerInvariant();
+                        if ((str2 == "rich text") || (str2 == "html"))
+                        {
+                            char[] trimChars = new char[] { ' ' };
+                            str = str.TrimEnd(trimChars);
+                        }
+                        foreach (FieldDescriptor descriptor in args.Options.Fields)
+                        {
+                            if (descriptor.FieldID == info.FieldID)
+                            {
+                                ItemUri uri = new ItemUri(info.ItemID, info.Language, info.Version, Factory.GetDatabase(descriptor.ItemUri.DatabaseName));
+                                if (descriptor.ItemUri == uri)
+                                {
+                                    #region patch 414532
+                                    if (descriptor.ContainsStandardValue)
+                                    {
+                                        var standardValuesItem = Factory.GetDatabase(descriptor.ItemUri.DatabaseName)?.GetItem(descriptor.ItemUri.ItemID);
+                                        if (standardValuesItem != null)
+                                        {
+                                            var standardValue = standardValuesItem.Fields?[descriptor.FieldID]?.Value ?? string.Empty;
+                                            if (str == standardValue)
+                                            {
+                                                unchangedFields.Add(descriptor);
+                                                continue;
+                                            }
+                                        }
+                                    }
+                                    #endregion
+                                    descriptor.Value = str;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            #region patch 414532
+            foreach (var id in unchangedFields)
+                args.Options.Fields.Remove(id);
+#endregion
+        }
+    }
+}
