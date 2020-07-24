18614c1ba2ff5e637fa83e038011c6800149e7f4
yevhen_povzlo <yevhen.povzlo@sitecore.com>
2020-07-24 09:33:33 +0300

    Creation derived clases from XmlBaseRenderingParser and ProcessXmlBasedLayoutDefinition

---
+++ src/Sitecore.Support.414532/NewXmlBasedRenderingParser.cs
@@ -0,0 +1,45 @@
+﻿namespace Sitecore.Support.Mvc.Presentation
+{
+
+using Sitecore.Data.Items;
+using Sitecore.Layouts;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Text;
+using Sitecore.Web;
+using System.Linq;
+
+
+    public class NewXmlBasedRenderingParser: XmlBasedRenderingParser
+    {
+        protected override void AddRenderingItemProperties(Rendering rendering)
+        {
+            RenderingItem renderingItem = rendering.RenderingItem;
+            if (renderingItem != null)
+            {
+                AddRenderingItemProperty(rendering, "DataSource", renderingItem.DataSource);
+                AddRenderingItemProperty(rendering, "Placeholder", renderingItem.Placeholder);
+                AddRenderingItemProperty(rendering, "Parameters", renderingItem.Parameters);
+                if (!rendering.Caching.Cacheable)
+                {
+                    RenderingCaching caching = renderingItem.Caching;
+                    rendering["Cacheable"] = caching.Cacheable.ToBoolString();
+                    rendering["Cache_VaryByData"] = caching.VaryByData.ToBoolString();
+                    rendering["Cache_VaryByDevice"] = caching.VaryByDevice.ToBoolString();
+                    rendering["Cache_VaryByLogin"] = caching.VaryByLogin.ToBoolString();
+                    rendering["Cache_VaryByParameters"] = caching.VaryByParm.ToBoolString();
+                    rendering["Cache_VaryByQueryString"] = caching.VaryByQueryString.ToBoolString();
+                    rendering["Cache_VaryByUser"] = caching.VaryByUser.ToBoolString();
+                }
+            }
+        }
+
+        protected new void AddRenderingItemProperty(Rendering rendering, string propertyName, string value)
+        {
+            if (!rendering.Properties[propertyName].ContainsText())
+            {
+                rendering[propertyName] = value;
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.414532/ProcessXmlBasedLayoutDefinition.cs
@@ -0,0 +1,32 @@
+﻿namespace Sitecore.Support.Mvc.Pipelines.Response.BuildPageDefinition
+{
+
+using Sitecore.Mvc.Configuration;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Pipelines.Response.BuildPageDefinition;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Support.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+
+    public class ProcessXmlBasedLayoutDefinition: Sitecore.Mvc.Pipelines.Response.BuildPageDefinition.ProcessXmlBasedLayoutDefinition
+    {
+
+        protected override IEnumerable<Rendering> GetRenderings(XElement layoutDefinition, BuildPageDefinitionArgs args)
+        {
+            XmlBasedRenderingParser parser = MvcSettings.GetRegisteredObject<XmlBasedRenderingParser>();
+            foreach (XElement deviceNode in layoutDefinition.Elements("d"))
+            {
+                Guid deviceId = deviceNode.GetAttributeValueOrEmpty("id").ToGuid();
+                Guid layoutId = deviceNode.GetAttributeValueOrEmpty("l").ToGuid();
+                yield return GetRendering(deviceNode, deviceId, layoutId, "Layout", parser);
+                foreach (XElement item in deviceNode.Elements("r"))
+                {
+                    yield return GetRendering(item, deviceId, layoutId, item.Name.LocalName, parser);
+                }
+            }
+        }
+    }
+}
